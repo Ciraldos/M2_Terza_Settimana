96c13d3d2c10b25f96547263edc79af2f4dd5953
Ciraldos <andreaciraldo2@gmail.com>
2024-05-07 16:22:33 +0200

    .

---
+++ Giorno_2/Esercitazione_S7_L2/index.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("content");
+
+const save = function(){
+    console.log("Premuto")
+   const inputValue=input.value;
+    localStorage.setItem("text-content", inputValue)
+}
+
+const deleteMemory = function(){
+    console.log("Premuto")
+    localStorage.removeItem("text-content")
+    input.value=""
+}
+
+window.onload = function() {
+    const savedText = localStorage.getItem("text-content");
+    if (savedText) {
+        input.value = savedText;
+    }
+};
+
+//contatore
+var contatore = sessionStorage.getItem('contatore') || 0;
+
+// Funzione per aggiornare il contatore e mostrare il tempo trascorso
+ function aggiornaContatore() {
+    contatore++; // Incrementa il contatore
+    document.getElementById("contatore").innerHTML = contatore + " secondi"; // Mostra il tempo trascorso
+    sessionStorage.setItem('contatore', contatore); // Salva il valore del contatore nella sessionStorage
+}
+
+// Avvia il contatore
+var intervallo = setInterval(aggiornaContatore, 1000);
