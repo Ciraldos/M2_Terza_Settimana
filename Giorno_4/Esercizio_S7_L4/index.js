3e2a16e1bd5749365aadc14bde2a9616429b27c6
Ciraldos <andreaciraldo2@gmail.com>
2024-05-09 18:12:46 +0200

    .

--- Giorno_4/Esercizio_S7_L4/index.js
+++ Giorno_4/Esercizio_S7_L4/index.js
@@ -1,35 +1,31 @@
-const generatePhotos = function(array) {
-    array.forEach((photo, i) => {
-        const photoElement = document.querySelectorAll(".card-img-top")[i];
-        console.log('Photo Element:', photoElement); // Aggiunto per debug
-        if (photoElement) {
-            photoElement.setAttribute("src", photo.url);
-        }
-    });
-}
 
 
-const getPhotos = function () {
-    fetch("https://api.pexels.com/v1/search?query=dog", { 
-        method: "GET", 
+const getPhotos = function (keyWord) {
+    fetch("https://api.pexels.com/v1/search?query=" + keyWord, {  
         headers: { 
-            Authorization: 'Bearer 4BBKISgFgHO4AXQ9wsFVeOWMXK50TP2dgogfDiiGruEYM6qr7jTG3HTQ'  
+            Authorization: "4BBKISgFgHO4AXQ9wsFVeOWMXK50TP2dgogfDiiGruEYM6qr7jTG3HTQ"  
         } 
     })
     .then((response) => {
+        console.log("Immagini caricate", response);
         if (response.ok) {
-            console.log("Immagini caricate", response);
             return response.json();
         } else {
             throw new Error("Le immagini non sono state caricate");
         }
     })
+
     .then((arrayOfPhotos) => {
-        console.log("Ho generato la lista delle foto", arrayOfPhotos);
-        generatePhotos(arrayOfPhotos.photos);
+        const allTheCardsImgs = document.querySelectorAll(".card img")
+        allTheCardsImgs.forEach((card, i)=>{
+            card.setAttribute("src", arrayOfPhotos.photos[i].src.medium)
+        })
+        
     })
     .catch((error) => {
         console.log("Errore", error);
     });
 }
+
+getPhotos("cockatiels")
 
